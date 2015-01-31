3063cd996909d6ccb5cdba5b2eec08534ab16654
Wilson Yeh <wilsonye@usc.edu>
2015-01-31 06:18:27 -0800

    starting sql

---
+++ TimeSync/sql.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+
+engine = create_engine('mysql://panda@localhost/TSDB')
+connection = engine.connect()
+
+
+
+"""
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+"""
